acc510192cd8c914c424c2bac5950fcf2eba052b
ismaelAleman2023 <ismael_aleman2023@hotmail.com>
2026-09-08 15:18:11 -0300

    agregado la clase articulo

---
+++ dominio/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Articulo
+    {
+        public Articulo() { }
+
+        public string Codigo_Articulo {  get; set; }
+        public string Nombre_Articulo {  get; set; }
+        public string Descripcion_Articulo {  get; set; }
+        public int IdMarca_Articulo { get; set; }
+        public int Categoria_Articulo {  get; set; }
+        public double Precio_Articulo { get; set; }
+
+    }
+
+}
