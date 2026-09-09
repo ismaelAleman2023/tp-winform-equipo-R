3cc2ae0beff05f82986a4837c1ebc195e0924796
Nicolas Aleman <nicolasaleman181203@gmail.com>
2026-09-09 16:52:18 -0300

    Agregado clase marca

---
+++ dominio/Marca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        public int Id_Marca{ get; }
+        public string Descripcion_Marca { get; set; }
+        public override string ToString()
+        {
+            return Descripcion_Marca;
+        }
+
+
+
+    }
+}
