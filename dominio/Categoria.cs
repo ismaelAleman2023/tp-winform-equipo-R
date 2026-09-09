86c34b198d3b2c5e8b415a43d9d46886cbccb34d
Nicolas Aleman <nicolasaleman181203@gmail.com>
2026-09-09 18:07:30 -0300

    Agregado clase Categoria

---
+++ dominio/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Categoria
+    {
+        public int Id_Categoria { get; }
+        public string Descripcion_Categoria { get; set; }
+
+        public override string ToString()
+        {
+            return Descripcion_Categoria;
+        }
+    }
+}
