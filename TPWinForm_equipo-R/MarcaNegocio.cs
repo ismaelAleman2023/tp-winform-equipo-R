e24a17fad4153aca2fa70d0e8a7f6ac3ffae616b
Nicolas Aleman <nicolasaleman181203@gmail.com>
2026-09-12 14:09:14 -0300

    Agregado buscar por Id en categoria negocio

---
+++ TPWinForm_equipo-R/MarcaNegocio.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class MarcaNegocio
+    {
+      
+        public void agregarMarca(Marca nuevoM)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.consultaSql("insert into marcas (Descripcion) select (@Descripcion)");
+                datos.setearParametros("@Descripcion", nuevoM.Descripcion_Marca);
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+        public List<Marca> listarMarcas()
+        {
+            List<Marca> lista_M = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.consultaSql("select id, descripcion from Marcas");
+                datos.ejecutarLetura();
+
+                 while (datos.Lector.Read())
+                {
+                    Marca marca = new Marca();
+                    marca.Id_Marca = (int) datos.Lector["id"];
+                    marca.Descripcion_Marca = (string)datos.Lector["descripcion"];
+
+                    lista_M.Add(marca);
+
+                }
+               
+                return lista_M;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+            
+
+
+        }
+
+        public Marca buscarId(int id)
+        {
+            Marca marca=new Marca();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.consultaSql("select descripcion from marcas where id = @id");
+                datos.setearParametros("@id", id);
+                datos.ejecutarLetura();
+
+                while (datos.Lector.Read())
+                {
+                    marca.Id_Marca = id;
+                    marca.Descripcion_Marca = (string)datos.Lector["descripcion"];
+                }
+
+                return marca;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally { datos.cerrarConexion(); }
+            
+        }
+
+        public bool eliminarMarca(int idMarca)
+        {
+            AccesoDatos datos =new AccesoDatos();
+
+            try
+            {
+                datos.consultaSql("delete from Marca where id = @id");
+                datos.setearParametros("@id", idMarca);
+                datos.ejecutarAccion();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { datos.cerrarConexion(); }
+
+        }
+
+       
+
+
+        public void modificarMarca(Marca marca)
+        {
+           
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.consultaSql("update Marca set descripcion =@descripcion where id = @id");
+                datos.setearParametros("@descripcion", marca.Descripcion_Marca);
+                datos.ejecutarAccion();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally 
+            { 
+                datos.cerrarConexion(); 
+            }
+
+        }
+
+
+
+    }
+
+} 
+
--- negocio/CategoriaNegocio.cs
+++ negocio/CategoriaNegocio.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using dominio;
@@ -52,9 +54,9 @@ namespace negocio
 
         }
 
-        public void agregarCategoria(Categoria nuevoC) { 
-        
-            AccesoDatos dato=new AccesoDatos();
+        public void agregarCategoria(Categoria nuevoC) {
+
+            AccesoDatos dato = new AccesoDatos();
 
             try
             {
@@ -70,13 +72,13 @@ namespace negocio
                 throw ex;
             }
             finally { dato.cerrarConexion(); }
-        
-        
+
+
         }
 
-        public bool eliminarCategoria(int idCategoria) { 
-         
-         AccesoDatos dato =new AccesoDatos();
+        public bool eliminarCategoria(int idCategoria) {
+
+            AccesoDatos dato = new AccesoDatos();
             try
             {
                 dato.consultaSql("delete from CATEGORIAS where Id = @id");
@@ -89,18 +91,41 @@ namespace negocio
             {
 
                 throw ex;
-                
+
             }
             finally { dato.cerrarConexion(); }
-        
-        
-        
+
+
+
         }
 
+        public Categoria buscarIdCategoria(int idCategoria) {
+
+            AccesoDatos dato = new AccesoDatos();
+            Categoria categoria = new Categoria();
+
+            try
+            {
+                dato.consultaSql("select descripcion from CATEGORIAS where id=@id");
+                dato.setearParametros("@id", idCategoria);
+
+                while (dato.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id_Categoria = idCategoria;
+                    aux.Descripcion_Categoria = (string)dato.Lector["descripcion"];
 
 
+                }
+                return categoria;
+            }
+            catch (Exception ex)
+            {
 
+                throw ex;
+            } finally { dato.cerrarConexion(); }
 
-
+        }
     }
 }
+
