5f64b554c12b5f9f16fc3cee6676c6aae66e403d
Nicolas Aleman <nicolasaleman181203@gmail.com>
2026-09-12 14:35:25 -0300

    Agregado clase Imagen

---
+++ dominio/Imagen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Activation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Imagen
+    {
+        public int Id_Imangen { get; set; }
+        public int IdArticulo_Imagen { get; set; }
+        public string Url_Imagen { get; set; }
+
+
+        public override string ToString()
+        {
+            return Url_Imagen;
+        }
+
+
+    }
+}
