d54a13a1e709fe0f28c0e261e05e26854f4537e9
Nicolas Aleman <nicolasaleman181203@gmail.com>
2026-09-15 17:12:18 -0300

    agregado WinForms adminCategoria , formulario categoría con Alta categoría

---
+++ TPWinForm_equipo-R/frmFormuCategoria.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TPWinForm_equipo_R
+{
+    partial class frmFormuCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTituloCate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTituloCate
+            // 
+            this.lbTituloCate.AutoSize = true;
+            this.lbTituloCate.BackColor = System.Drawing.Color.Yellow;
+            this.lbTituloCate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTituloCate.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTituloCate.Location = new System.Drawing.Point(131, 27);
+            this.lbTituloCate.Name = "lbTituloCate";
+            this.lbTituloCate.Size = new System.Drawing.Size(325, 44);
+            this.lbTituloCate.TabIndex = 0;
+            this.lbTituloCate.Text = "Agregar Categoria";
+            this.lbTituloCate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Descripcion :";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(279, 220);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(135, 20);
+            this.txtDescripcion.TabIndex = 4;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(363, 373);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(131, 35);
+            this.btnConfirmar.TabIndex = 5;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(85, 373);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(131, 35);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(355, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Su Id se asignara automaticamente ";
+            // 
+            // frmFormuCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 494);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbTituloCate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(563, 494);
+            this.MinimumSize = new System.Drawing.Size(563, 494);
+            this.Name = "frmFormuCategoria";
+            this.Text = "frmFormuCategoria";
+            this.Load += new System.EventHandler(this.frmFormuCategoria_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTituloCate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ TPWinForm_equipo-R/frmFormuCategoria.cs
@@ -0,0 +1,60 @@
+﻿using dominio;
+using negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPWinForm_equipo_R
+{
+    public partial class frmFormuCategoria : Form
+    {
+        public frmFormuCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmFormuCategoria_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            Categoria cate=new Categoria();
+            CategoriaNegocio catenego=new CategoriaNegocio();
+            
+            try
+            {
+
+            string descrip=txtDescripcion.Text;
+            cate.Descripcion_Categoria=descrip;
+                catenego.agregarCategoria(cate);
+                MessageBox.Show("Agregado Correctamente");
+                Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
